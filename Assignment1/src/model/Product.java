393ef104f656c0c0a33cfd7ad71d942f8bb51dd0
Your Name <your.email@example.com>
2024-09-17 00:08:25 -0400

    Add Assignment1 folder with files

---
+++ Assignment1/src/model/Product.java
@@ -0,0 +1,196 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author chw
+ */
+public class Product {
+    String Firstname;
+    String Lastname;
+    String Email;
+    String Phone;
+    String DriverLN;
+    String DriverLS;
+    String Social;
+    String Address1;
+    String Address2;
+    String City;
+    String State;
+    String Zip;
+    String TelephoneNumber;
+    String FaxNumber;
+    String Academic;
+    String Height;
+    String Blood;
+    String Favorite;
+    String Weight;
+    String Age;
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String Firstname) {
+        this.Firstname = Firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String Lastname) {
+        this.Lastname = Lastname;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDriverLN() {
+        return DriverLN;
+    }
+
+    public void setDriverLN(String DriverLN) {
+        this.DriverLN = DriverLN;
+    }
+
+    public String getDriverLS() {
+        return DriverLS;
+    }
+
+    public void setDriverLS(String DriverLS) {
+        this.DriverLS = DriverLS;
+    }
+
+    public String getSocial() {
+        return Social;
+    }
+
+    public void setSocial(String Social) {
+        this.Social = Social;
+    }
+
+    public String getAddress1() {
+        return Address1;
+    }
+
+    public void setAddress1(String Address1) {
+        this.Address1 = Address1;
+    }
+
+    public String getAddress2() {
+        return Address2;
+    }
+
+    public void setAddress2(String Address2) {
+        this.Address2 = Address2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZip() {
+        return Zip;
+    }
+
+    public void setZip(String Zip) {
+        this.Zip = Zip;
+    }
+
+    public String getTelephoneNumber() {
+        return TelephoneNumber;
+    }
+
+    public void setTelephoneNumber(String TelephoneNumber) {
+        this.TelephoneNumber = TelephoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return FaxNumber;
+    }
+
+    public void setFaxNumber(String FaxNumber) {
+        this.FaxNumber = FaxNumber;
+    }
+
+    public String getAcademic() {
+        return Academic;
+    }
+
+    public void setAcademic(String Academic) {
+        this.Academic = Academic;
+    }
+
+    public String getHeight() {
+        return Height;
+    }
+
+    public void setHeight(String Height) {
+        this.Height = Height;
+    }
+
+    public String getBlood() {
+        return Blood;
+    }
+
+    public void setBlood(String Blood) {
+        this.Blood = Blood;
+    }
+
+    public String getFavorite() {
+        return Favorite;
+    }
+
+    public void setFavorite(String Favorite) {
+        this.Favorite = Favorite;
+    }
+
+    public String getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(String Weight) {
+        this.Weight = Weight;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+    
+
+    
+    
+}
